4d6ebbd0728d6737060516f6f833018dea73d2ea
NohaAhmed01 <nohaniho88@gmail.com>
2026-08-06 05:20:47 +0300

    Fixed the routing to products page to first pagination in an easier way

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
     return (
         <ul className={styles.navbar}>
             <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="products">Products</NavLink></li>
+            <li><NavLink to="products?page=1">Products</NavLink></li>
             <li><NavLink to="checkout">Checkout</NavLink></li>
             <li><NavLink to="about">About</NavLink></li>
         </ul>
